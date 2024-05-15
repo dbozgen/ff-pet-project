52eb3ed1f6208cf5d3a5b525e404055677ed0648
dogukan_o <dogukan.ozgen@gmail.com>
2024-05-14 18:07:10 -0700

    added factory functions

--- ffunction-pet-project/js/script.js
+++ ffunction-pet-project/js/script.js
@@ -1,2 +1,39 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
+
+const createPet = function (name, species){
+    const pet = {
+        name:name,
+        species:species,
+        isTired:5,
+        sleep:function(){
+            console.log(`${this.name} needs a nap. Zzzz...`);
+            this.isTired = 1;
+        },
+        play:function(){
+            if (this.isTired === 10){
+                console.log("Too tired to play");
+                this.sleep();
+            } else{
+                console.log(`Yay! ${name} loves to play!`);
+                this.isTired += 1;
+            }
+        },
+    };
+    return pet;
+
+};
+
+const sora = createPet("Sora", "ferret");
+const clover = createPet("Clover", "rabbit");
+const baxter = createPet("Baxter","hamster");
+const cleo = createPet("Cleo","rat");
+const francine = createPet("Francine","turtle");
+
+// console.log (sora, clover,cleo, baxter,francine);
+
+clover.sleep();
+baxter.play();
+
+console.log(clover);
+console.log(baxter);
