6305470093acd59d59197065ea63488bf9aa6168
dogukan_o <dogukan.ozgen@gmail.com>
2024-05-14 18:36:37 -0700

    displayed pet objects

--- ffunction-pet-project/js/script.js
+++ ffunction-pet-project/js/script.js
@@ -32,8 +32,34 @@ const francine = createPet("Francine","turtle");
 
 // console.log (sora, clover,cleo, baxter,francine);
 
-clover.sleep();
-baxter.play();
+// clover.sleep();
+// baxter.play();
 
-console.log(clover);
-console.log(baxter);
+// console.log(clover);
+// console.log(baxter);
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+let allPets = [sora, clover, baxter, cleo, francine];
+// console.log (allPets);
+
+const showPets = function(petArray){
+    pets.innerHTML = "";
+
+    for (let pet of petArray){
+        let status = "ready to play!";
+        if (pet.isTired >= 7){
+            status = "sleeping.";
+        }
+        const li = document.createElement("li");
+        li.innerHTML = `<span class="pet-name">${pet.name} </span> the ${pet.species} is ${status}`;
+        pets.append(li);
+    }
+};
+
+statusButton.addEventListener("click", 
+function(){
+    showPets(allPets);
+}
+);
